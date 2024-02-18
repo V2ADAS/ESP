fc5a50c6eca8ccfce3081df16ab0a61ab0976e16
MayaEmad <128156953+MayaEmad@users.noreply.github.com>
2024-02-18 06:41:38 +0200

    Add files via upload

---
+++ ESP32_Receive.ino
@@ -0,0 +1,15 @@
+#include <HardwareSerial.h>
+HardwareSerial SerialPort(2);
+void setup() {
+  // put your setup code here, to run once:
+SerialPort.begin(9600,SERIAL_8N1,16,17);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (SerialPort.available()){
+  String Buffer = SerialPort.readString();   // data type to be received example
+  Serial.print(Buffer);
+  }
+}
